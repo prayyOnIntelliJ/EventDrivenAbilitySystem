38b480e2c4ebd21f0a6940e08fa5f43a7b839837
prayy <122681300+prayyOnIntelliJ@users.noreply.github.com>
2026-02-02 20:22:33 +0100

    Added the AbilityBase Class

---
+++ Source/EventAbilitySystem/AbilityBase.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "UObject/NoExportTypes.h"
+#include "AbilityBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class EVENTABILITYSYSTEM_API UAbilityBase : public UObject
+{
+	GENERATED_BODY()
+	
+};
