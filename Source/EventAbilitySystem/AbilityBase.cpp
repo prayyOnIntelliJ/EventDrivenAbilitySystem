15b004cda6e373211fa770c677dc3ab614c91a55
prayy <122681300+prayyOnIntelliJ@users.noreply.github.com>
2026-02-03 19:06:28 +0100

    Added base functions and properties for AbilityBase

--- Source/EventAbilitySystem/AbilityBase.cpp
+++ Source/EventAbilitySystem/AbilityBase.cpp
@@ -3,3 +3,29 @@
 
 #include "AbilityBase.h"
 
+bool UAbilityBase::CanActivate() const
+{
+	return true;
+}
+
+void UAbilityBase::Activate()
+{
+	if (CanActivate())
+	{
+		bIsActive = true;
+		OnActivate():
+	}
+}
+
+bool UAbilityBase::IsOnCooldown() const
+{
+	return false;
+}
+
+void UAbilityBase::End()
+{
+}
+
+void UAbilityBase::OnActivate()
+{
+}
--- Source/EventAbilitySystem/AbilityBase.h
+++ Source/EventAbilitySystem/AbilityBase.h
@@ -3,15 +3,33 @@
 #pragma once
 
 #include "CoreMinimal.h"
-#include "UObject/NoExportTypes.h"
 #include "AbilityBase.generated.h"
 
-/**
- * 
- */
 UCLASS()
 class EVENTABILITYSYSTEM_API UAbilityBase : public UObject
 {
 	GENERATED_BODY()
 	
+public:
+	virtual bool CanActivate() const;
+	void Activate();
+	FORCEINLINE bool IsActive() const { return bIsActive; }
+	bool IsOnCooldown() const;
+	virtual void End();
+	
+private:
+	UPROPERTY()
+	ACharacter* Owner;
+	
+	UPROPERTY()
+	bool bIsActive = false;
+	
+	virtual void OnActivate();
+	
+	FTimerHandle ActivationTimerHandle;
+	
+	void StartCooldown();
+	void ResetCooldown();
+	FORCEINLINE ACharacter* GetOwnerCharacter() const { return Owner; }	
 };
+
